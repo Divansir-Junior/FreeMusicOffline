20524eed48e426b6b9091bdfff0adc2411105788
Divansir-Junior <jscrobut989@gmail.com>
2026-02-13 01:00:28 -0300

    feat : MediaStoreDataSource class  atributes and functions created

--- app/src/main/java/com/example/musicoffline/data/datasource/MediaStoreDataSource.kt
+++ app/src/main/java/com/example/musicoffline/data/datasource/MediaStoreDataSource.kt
@@ -1,4 +1,55 @@
 package com.example.musicoffline.data.datasource
 
-class MediaStoreDataSource {
+import android.content.ContentResolver
+import android.provider.MediaStore
+import com.example.musicoffline.data.model.AudioModel
+
+class MediaStoreDataSource(
+    private val contentResolver: ContentResolver
+) {
+
+    fun getAllMusic(): List<AudioModel> {
+        val musicList = mutableListOf<AudioModel>()
+
+        val projection = arrayOf(
+            MediaStore.Audio.Media._ID,
+            MediaStore.Audio.Media.TITLE,
+            MediaStore.Audio.Media.ARTIST,
+            MediaStore.Audio.Media.DURATION
+        )
+
+        val selection = "${MediaStore.Audio.Media.IS_MUSIC} != 0"
+
+        val cursor = contentResolver.query(
+            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            selection,
+            null,
+            null
+        )
+
+        cursor?.use {
+            val idColumn = it.getColumnIndexOrThrow(MediaStore.Audio.Media._ID)
+            val titleColumn = it.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE)
+            val artistColumn = it.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST)
+            val durationColumn = it.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION)
+
+            while (it.moveToNext()) {
+                val id = it.getLong(idColumn)
+                val title = it.getString(titleColumn)
+                val artist = it.getString(artistColumn)
+                val duration = it.getLong(durationColumn)
+
+                val uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI.buildUpon()
+                    .appendPath(id.toString())
+                    .build()
+
+                musicList.add(
+                    AudioModel(id, title, artist, duration, uri)
+                )
+            }
+        }
+
+        return musicList
+    }
 }
