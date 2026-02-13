dae5ca8eda87f6e610a186c758c49017967ba10f
Divansir-Junior <jscrobut989@gmail.com>
2026-02-13 01:01:44 -0300

    feat : Get all musics created

--- app/src/main/java/com/example/musicoffline/data/repository/MusicRepository.kt
+++ app/src/main/java/com/example/musicoffline/data/repository/MusicRepository.kt
@@ -1,4 +1,13 @@
 package com.example.musicoffline.data.repository
 
-class MusicRepository {
+import com.example.musicoffline.data.datasource.MediaStoreDataSource
+import com.example.musicoffline.data.model.AudioModel
+
+class MusicRepository(
+    private val dataSource: MediaStoreDataSource
+) {
+
+    fun getAllMusic(): List<AudioModel> {
+        return dataSource.getAllMusic()
+    }
 }
